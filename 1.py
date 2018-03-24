51813a2895ae2688cf74512b615160ca911de3d6
Hitch67 <18042757+hitch67@users.noreply.github.com>
2018-03-24 10:08:41 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,11 @@
+nums = [2, 7, 11, 15]
+target = 9
 
-
-
-
-def
+def twoSum(self, nums, target):
+    d = {}
+    for i,n in enumerate(nums):
+        m = target - n
+        if m in d:
+            return [d[m],i]
+        else:
+            d[n] = i
