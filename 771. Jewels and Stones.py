761ee61f8f01d221a217ca564c24b80dda1411cd
Hitch67 <18042757+hitch67@users.noreply.github.com>
2018-04-10 09:51:34 +0800

    Create 771. Jewels and Stones.py

---
+++ 771. Jewels and Stones.py
@@ -0,0 +1,10 @@
+#so fucking easy!
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum(map(J.count,S))
+    
